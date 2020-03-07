d5596a8f0cc4a152e9f4c2f9a2159f2223c0702f
Felipe Aurelio <felipeaureliotesta@gmail.com>
2020-03-07 20:57:16 -0300

    atv3, 4 e 5 done

--- atv.c
+++ atv.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <math.h>
 
+/*
+mudar na main qual a atv quer testar
+executar passando uma entrada com <
+atv1 < entrada1
+atv2, atv3, atv4 < entrada2
+atv5 < entrada3
+*/
+
 void atv1() {
 
     /*
@@ -17,6 +25,7 @@ void atv1() {
         int e1[s];
         int e2[s];
         int r[s];
+
         //le as entrys 1 e 2
         for(int x = 0; x < s; x++){
             scanf("%d", &e1[x]);
@@ -24,6 +33,7 @@ void atv1() {
         for(int x = 0; x < s; x++){
             scanf("%d", &e2[x]);
         }
+
         #pragma omp parallel
         {
             //for para fazer a distribuicao das threads pelo vetor
@@ -40,6 +50,8 @@ void atv1() {
 }
 
 void atv2(){
+
+    //primeiro elemento da entrada corresponde ao numero de elementos, o resto da entrada sao os elementos
  
   int s;
 
@@ -49,10 +61,13 @@ void atv2(){
       double desvio = 0;
       double media;
       double t = 0;
+
+      //le os elementos
       for(int x = 0; x < s; x++){
           scanf("%lf", &v[x]);
       }
       
+    //somatoria para calcular media
     #pragma pragma omp parallel for reduction(+:r)
     {
         for(int x = 0; x < s; x++){
@@ -60,7 +75,8 @@ void atv2(){
         }
     }
     media = r / s;
-    printf("%lf\n", media);
+    printf("media: %lf\n", media);
+    //somatoria para calcular desvio
     #pragma pragma omp parallel for reduction(+:t)
     {
         for(int x = 0; x < s; x++){
@@ -68,10 +84,118 @@ void atv2(){
         }
     }
     desvio = sqrt(t/s);
-    printf("%lf\n", desvio);  
+    printf("desvio: %lf\n", desvio);  
   }   
 }
 
+void atv3(){
+    
+    //primeiro elemento da entrada corresponde ao numero de elementos, o resto da entrada sao os elementos
+ 
+    int s;
+
+    while(scanf("%d", &s) != EOF){
+        double v[s];
+        double si = 0; 
+        double mh;
+
+        //le os elementos
+        for(int x = 0; x < s; x++){
+          scanf("%lf", &v[x]);
+        }
+
+        //calculo da somatoria dos valores invertidos
+        #pragma pragma omp parallel for reduction(+:r)
+        {
+            for(int x = 0; x < s; x++){
+                si += 1.0/v[x];
+            }
+        }
+        //calculo da media harmonica. cast para nao perder decimal
+        mh = (double)s/si;
+        printf("media harmonica: %lf\n", mh);
+    }
+
+}
+
+void atv4(){
+    
+    //primeiro elemento da entrada corresponde ao numero de elementos, o resto da entrada sao os elementos
+ 
+    int s;
+
+    while(scanf("%d", &s) != EOF){
+        double v[s];
+        double min = 9999;
+        double max = 0;
+
+        //le os elementos
+        for(int x = 0; x < s; x++){
+          scanf("%lf", &v[x]);
+        }
+
+        #pragma pragma omp parallel for reduction(min:r)
+        {
+            for(int x = 0; x < s; x++){
+                min = min(min, v[x]);
+            }   
+        }
+        #pragma pragma omp parallel for reduction(max:r)
+        {
+            for(int x = 0; x < s; x++){
+                max = max(max, v[x]);
+            }  
+        }
+        printf("maior: %lf\nmenor: %lf\n", max, min);
+
+  }  
+
+}
+
+atv5(){
+
+    //algoritmo para mostrar primos de 0 ate n utilizando crivo de erastoteles
+    int n;
+
+    scanf("%d", &n);
+     
+    //se primos[x] == 1 entao eh primo
+    int primos[n+1]; 
+
+    #pragma omp parallel
+    {
+        
+        #pragma omp for
+        for(int x = 0; x < n; x++){
+            primos[x] = 1;
+        }
+    }
+
+
+    #pragma omp parallel
+    {
+        #pragma omp for
+        for (int p = 2; p * p <= n; p++){ 
+            
+            if (primos[p] == 1) 
+            { 
+                // atualiza todos os multiplos de p maiores que sua raiz quadrada.
+                // numeros multiplos de p menores que sua raiz quadrada ja foram analisados.  
+                #pragma omp for
+                for (int i = p * p; i <= n; i += p) 
+                    primos[i] = 0; 
+            } 
+        } 
+    }
+
+  
+    // printa os primos
+    for (int p = 2; p <= n; p++) 
+       if (primos[p]) 
+          printf("%d\n", p); 
+} 
+  
+
 void main() {
-    atv2();
+    
 }
